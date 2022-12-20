5049867f7218212ab6e7e30295135afd18e753b7
ViktoriyaVlasova <71847067+ViktoriyaVlasova@users.noreply.github.com>
2022-12-20 10:01:49 +0300

    Add files via upload

---
+++ cinema/cinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinema
+//
+//  Created by Власова Виктория Михайловна on 14/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
